8d5cc37c1dede03a77b6b48ab823b3859a9ea9f5
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-07-30 18:22:43 +0500

    Added Fetch Api and processed Data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
-
+import React, { useEffect, useState } from "react";
 function App() {
-  return <div className="App">Hello</div>;
+  let data = { title: "Waiting for the data" };
+  const [todo, setTodo] = useState(data);
+  const [isFetching, setFetching] = useState(false);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    setFetching(true);
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/1`
+    );
+    console.log("response = ", response);
+    let data = await response.json();
+    setTodo(data);
+    setFetching(false);
+    console.log("Data = ", data);
+  }
+
+  if (isFetching) {
+    return <div>Data Loading......</div>;
+  }
+  return (
+    <div className="App">
+      Hello
+      <span>Title : {todo.title}</span>
+    </div>
+  );
 }
 
 export default App;
